1188c81a6f5ad3c4181e25e253ce699b38887dde
Lucien Leroy, Siani <lucienleroy.siani@dbh.de>
2022-09-22 11:15:08 +0200

    creating entity models

---
+++ Models/Custom.cs
@@ -0,0 +1,14 @@
+﻿namespace ef_core.Models;
+
+public class Custom
+{
+	public int Id { get; set; }
+	public string FirstName { get; set; } = null!;
+	public string LastName { get; set; } = null!;
+	public string? Address { get; set; }
+	public string? Phone { get; set; }
+
+	public ICollection<Order> Orders { get; set; } = null!;
+
+
+}
---
+++ Models/OrderDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace ef_core.Models;
+
+public class OrderDetail
+{
+	public int Id { get; set; }
+	public int Quantity { get; set; }
+	public int OrderId { get; set; }
+	public int ProductId { get; set; }
+	public Product Product { get; set; } = null!;
+	public Order Order { get; set; } = null!;
+}
